acee1285669e8a0eb89db8875f64ffa6a00541bf
Pedro Henrique Medeiros de Queiroga <pedro_q777@live.com>
2024-03-20 23:16:49 -0300

    Adicionando os arquivos do site
    Primeira vesão do site que contem os arquivos já com o desafio concluído.

---
+++ script.js
@@ -0,0 +1,79 @@
+let keyWords = new Map([
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"]
+]);
+
+
+function getInputText(actionCrypt) {
+    let textarea = document.querySelector('textarea');
+
+    if (actionCrypt === 'encrypt')
+        encrypt(keyWords, textarea.value);
+    else {
+        decrypt(keyWords, textarea.value);
+    }
+}
+
+function encrypt (keyWords, input) {
+    let inputArray = input.split('');
+
+    for (let i = 0; i < inputArray.length; i++) {
+        keyWords.forEach((valor, chave) => {
+            if (inputArray[i] == chave) {
+                inputArray[i] = valor;
+            }
+        });
+    }
+    
+    resultView(inputArray.join(''));
+}
+
+function decrypt (keyWords, input) {
+    let encoded = false;
+
+    for (let valor of keyWords.values()) {
+        if (input.includes(valor)) {
+            encoded = true;
+            break;
+        }
+    }
+
+    if (!encoded) {
+        alert('Esse texto ainda não foi codificado!');
+        return;
+    }
+
+    let inputArray = input.split('');
+
+    for (let i = 0; i < inputArray.length; i++) {
+        keyWords.forEach((valor, chave) => {
+            if (inputArray[i] == chave) {
+                for (let j = 1; j < valor.split('').length; j++) {
+                    inputArray[i+j] = '';
+                }
+            }
+        });
+    }
+    
+    resultView(inputArray.join(''));
+}
+
+function resultView (resultText) {
+    let resultTextView = document.getElementById('output-area__result-area__result-text');
+    resultTextView.textContent = resultText;
+}
+
+async function copyToTransferArea () {
+    let copyButton = document.getElementById('output-area__result-area__result-text');
+    const copyText = copyButton.textContent;
+
+    try {
+        await navigator.clipboard.writeText(copyText);
+        alert('Texto copiado para a área de transferência!');
+    } catch (error) {
+        console.error('Erro ao copiar texto:', error);
+    }
+}
